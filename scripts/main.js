bca869a8604bee04766010d44a5a88ff86492130
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2025-08-29 21:34:00 -0400

    cria overlay e cards para preencher espaço nas tec

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,24 @@
 const menu = document.querySelector(".menu");
 const botaoMenu = document.querySelector(".btn-menu");
 const iconeMenu = document.querySelector(".fa-solid");
+const overlayParaMenu = document.querySelector(".overlay");
 
 botaoMenu.addEventListener("click", () => {
     trocaIcone();
-    showMenu()
+    showMenu();
 });
 
 const trocaIcone = () => {
     if (iconeMenu.classList.contains("fa-bars")) {
         iconeMenu.classList.remove("fa-bars");
         iconeMenu.classList.add("fa-xmark");
-    }else{
+    } else {
         iconeMenu.classList.remove("fa-xmark");
         iconeMenu.classList.add("fa-bars");
     }
 };
 
-const showMenu = ()=>{
-    menu.classList.toggle("active")
-}
+const showMenu = () => {
+    menu.classList.toggle("active");
+    overlayParaMenu.classList.toggle("active")
+};
